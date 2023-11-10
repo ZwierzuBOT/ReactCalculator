23f715eaf2407f0a8227cb3936e66bcbb352ff0e
Mateusz Zwierzchowski <125812607+ZwierzuBOT@users.noreply.github.com>
2023-11-10 20:36:16 +0100

    Update App.js

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ const [result, setResult] = useState("");
 
 const addToText = (val) => {
   setText((text) => [...text, val])
+  console.log("dsadas")
 };
 
 const resetInput = () => {
@@ -27,6 +28,16 @@ const calculateResult = () => {
   setResult(math.evaluate(input));
 }
 
+
+const plusMinus = () => {
+  const input = text.join("");
+
+  setResult(-math.evaluate(input))
+
+}
+
+
+
   return (
     <div className="App">
       <div className="calc-wrapper">
@@ -34,18 +45,21 @@ const calculateResult = () => {
       <Input text={text} result={result}/>
         <div className="row">
           <Button symbol={"AC"} color={anotherColor} handleClick={resetInput}/>
+          <Button symbol={"±"} color={anotherColor} handleClick={plusMinus}/>
+          <Button symbol={"%"} color={anotherColor} handleClick={addToText}/>
+          <Button symbol={"/"} color={buttonColor} handleClick={addToText}/>
         </div>
         <div className="row">
        <Button symbol={"7"} handleClick={addToText}/>
        <Button symbol={"8"} handleClick={addToText}/>
        <Button symbol={"9"} handleClick={addToText}/>
-       <Button symbol={"/"} color={buttonColor} handleClick={addToText}/>
+       <Button symbol={"*"} color={buttonColor} handleClick={addToText}/>
         </div>
         <div className="row">
        <Button symbol={"4"} handleClick={addToText}/>
        <Button symbol={"5"} handleClick={addToText}/>
        <Button symbol={"6"} handleClick={addToText}/>
-       <Button symbol={"*"} color={buttonColor} handleClick={addToText}/>
+       <Button id={"minus"} color={buttonColor} symbol={"-"} handleClick={addToText}/>
         </div>
         <div className="row">
        <Button symbol={"1"} handleClick={addToText}/>
@@ -55,7 +69,6 @@ const calculateResult = () => {
         </div>
         <div className="row">
        <Button id={"zero"}symbol={"0"} handleClick={addToText}/>
-       <Button id={"minus"}symbol={"-"} handleClick={addToText}/>
        <Button id={"equals"}symbol={"="} color={buttonColor} handleClick={calculateResult}/>
         </div>
       </div>
